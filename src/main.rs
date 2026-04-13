db7c650e21b21c3d404690e75f78edb8eb3ef628
Kayla Bevans <joshprince230@yahoo.com>
2026-04-13 19:22:42 -0400

    Added CORS layer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate argon2;
 use std::net::SocketAddr;
-use axum::{Router, routing::post};
+
+use axum::{Router, http::HeaderValue, routing::post};
 use chrono::Utc;
-use tracing::info;
+use reqwest::{Method, header::CONTENT_TYPE};
+use tower_http::cors::CorsLayer;
+use tracing::{info, error};
 
 use crate::routes::{SharedState, post as PostRoutes};
 
@@ -16,11 +19,26 @@ async fn main() {
     info!("Tracing initialized; constructing SharedState at {}", Utc::now());
     let state = match SharedState::new().await {
         Ok(state) => state,
-        Err(error) => panic!("Could not create shared state => {error:?}")
+        Err(error) => {
+            error!("Could not create shared state => {error:?}");
+
+            return ()
+        }
     };
-    let rustls_config = state.rustls_config.clone();
 
     info!("Attempting to build Router at {}", Utc::now());
+    let allowed_origin = match HeaderValue::try_from("*") {
+        Ok(origin) => origin,
+        Err(error) => {
+            error!("Could not get HeaderValue from provided input => {error}");
+
+            return ()
+        }
+    };
+    let cors = CorsLayer::new()
+        .allow_headers([CONTENT_TYPE])
+        .allow_origin(allowed_origin)
+        .allow_methods(Method::POST);
     let router = Router::new()
         .route("/add_user", post(PostRoutes::create_user))
         .route("/login", post(PostRoutes::basic_login))
@@ -32,13 +50,14 @@ async fn main() {
         .route("/case/notes/add", post(PostRoutes::insert_note))
         .route("/case/find/all", post(PostRoutes::find_accessible_cases))
         .route("/case/notes/find/all", post(PostRoutes::find_accessible_notes))
-        .with_state(state);
+        .with_state(state.clone())
+        .layer(cors);
 
     info!("Attempting to create TCP listener at {}", Utc::now());
     let listener = SocketAddr::from(([0, 0, 0, 0], 8443));
 
-    info!("Serving over HTTPS listener at {}", Utc::now());
-    let _ = axum_server::bind_rustls(listener, rustls_config)
+    info!("Serving listener at {}", Utc::now());
+    let _ = axum_server::bind_rustls(listener, state.rustls_config)
         .serve(router.into_make_service())
         .await;
 }
